b7455ad269506fcbb9ba291496fa236ca864ac97
Ralphy89 <43939986+ralphy89@users.noreply.github.com>
2024-05-04 16:49:29 -0400

    Update models.py

--- myPortFolio/app/models.py
+++ myPortFolio/app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-# Ralphy89..
+
 class User(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
