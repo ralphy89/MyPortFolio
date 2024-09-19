c1581d584a285284200624a432b6547fda9e9bf7
Ralphy89 <43939986+ralphy89@users.noreply.github.com>
2024-09-19 18:51:02 -0400

    Merge remote-tracking branch 'origin/main'

--- myPortFolio/build_files.sh
+++ myPortFolio/build_files.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "BUILD START"
 
-# create a virtual environment named 'venv' if it doesn't already exist
-python3.9 -m venv venv
-
-# activate the virtual environment
-source venv/bin/activate
-pip install -r requirements.txt
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
 python3.9 manage.py collectstatic --noinput
