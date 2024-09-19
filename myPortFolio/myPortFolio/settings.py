d31b4c12c813e2c50a8791e5acbb358f1787e2ff
Ralphy89 <43939986+ralphy89@users.noreply.github.com>
2024-09-19 18:08:57 -0400

    Merge remote-tracking branch 'origin/main'

--- myPortFolio/myPortFolio/settings.py
+++ myPortFolio/myPortFolio/settings.py
@@ -122,7 +122,6 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
