ce50ef6fccaf95b0b347a727499d92fb4b61d1c9
Ralphy89 <43939986+ralphy89@users.noreply.github.com>
2024-09-19 20:43:20 -0400

    Merge remote-tracking branch 'origin/main'

--- myPortFolio/myPortFolio/urls.py
+++ myPortFolio/myPortFolio/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from myPortFolio.myPortFolio import settings
 
 urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
-
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
